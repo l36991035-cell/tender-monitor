9cc9980a29e34a2826704a7680ef46d6b3468958
User <l36991035@gmail.com>
2026-06-06 23:01:10 +0800

    feat: fill in Apps Script URL in config.js

--- docs/config.js
+++ docs/config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
   SPREADSHEET_ID: '1kY9-FnRKQxAACcfn1CIb1rYkrlSkYevNbnsvcQBvTB0',
   GOOGLE_API_KEY: 'AIzaSyDgsEXfuyPB08y1CJQcPofWKrIvIH4qPBI',
-  APPS_SCRIPT_URL: '',
+  APPS_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbwbLf_R6iq6byI865Z2LCFUcjtwVM981lejixIK_eJuXHVaxTtuw709We-Z8U584xdK/exec',
 };
