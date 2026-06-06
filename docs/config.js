1e392d9df6d23e479781b1fe235d97d26c0c9c7b
User <l36991035@gmail.com>
2026-06-06 23:54:48 +0800

    feat: add delete_watching support, update Apps Script URL

--- docs/config.js
+++ docs/config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
   SPREADSHEET_ID: '1kY9-FnRKQxAACcfn1CIb1rYkrlSkYevNbnsvcQBvTB0',
   GOOGLE_API_KEY: 'AIzaSyDgsEXfuyPB08y1CJQcPofWKrIvIH4qPBI',
-  APPS_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycby5Ea7ddoYCVuovDAYEDiWHgJM7lF9YDwsT67toUZgYeImPFDTToGClDcmVz-G_RXsm/exec',
+  APPS_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbzwPqzi3CwI5rsHlyH0yazB8srYKsEmcdHsOqpgdn04iGKPDYRI2QJJfaAGFWEK42_E/exec',
 };
